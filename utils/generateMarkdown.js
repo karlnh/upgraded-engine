f5ed0be3f4009506fa80e76bc20d0fcde376c6cc
karlnh <hubigkarl@gmail.com>
2022-12-31 22:39:21 -0600

    add markdown for generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +14,42 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  [![License](!${renderLicenseBadge(data.license)}](${renderLicenseLink(data.license)})
 
-`;
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution Guidelines](#contributing)
+  - [Testing Instructions](#testing)
+  - [License](#license)
+  - [Further Questions](#contact)
+  
+  ## Description
+  ${data.desc}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution Guidelines
+  ${data.contribute}
+
+  ## Testing Instructions
+  ${data.testing}
+
+  ## License
+  [![${data.license}](${renderLicenseLink(data.license)})](${renderLicenseSection(data.license)})
+
+  ## Further Questions
+  Direct any further questions to one of the following:
+  [${data.email}](mmailto:${data.email})
+  [https://github.com/${data.user}](https://github.com/${data.user})
+`
 }
 
 module.exports = generateMarkdown;
